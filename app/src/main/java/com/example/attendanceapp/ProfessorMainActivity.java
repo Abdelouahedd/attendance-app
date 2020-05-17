912900e21037d3a1b1b3f976e60b35b94eceaee2
ablil <ablil@protonmail.com>
2020-05-17 00:28:51 +0000

    add home fragment

--- app/src/main/java/com/example/attendanceapp/ProfessorMainActivity.java
+++ app/src/main/java/com/example/attendanceapp/ProfessorMainActivity.java
@@ -1,14 +1,37 @@
 package com.example.attendanceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfessorMainActivity extends AppCompatActivity {
 
+    BottomNavigationView navMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor_main);
+
+        navMenu = findViewById(R.id.bottom_nav_menu);
+        navMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = new HomeFragment();
+
+                if (item.getItemId() == R.id.nav_home) {
+                    fragment = new HomeFragment();
+                } 
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+
+                return true;
+            }
+        });
     }
 }
