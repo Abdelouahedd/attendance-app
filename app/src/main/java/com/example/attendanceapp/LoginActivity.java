ac242d36df6a459616df7cb08958c8a9a823b22a
ablil <ablil@protonmail.com>
2020-06-20 21:53:52 +0100

    feat (auth): add authentication and firebase connection

--- app/src/main/java/com/example/attendanceapp/LoginActivity.java
+++ app/src/main/java/com/example/attendanceapp/LoginActivity.java
@@ -1,30 +1,84 @@
 package com.example.attendanceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     MaterialButton loginButton;
+    private FirebaseAuth mAuth;
+    private TextInputEditText usernameInput;
+    private TextInputEditText passwordInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // TODO: implemtnt button listener and remove this
+        mAuth = FirebaseAuth.getInstance();
         loginButton = findViewById(R.id.login_button);
+        usernameInput = findViewById(R.id.login);
+        passwordInput = findViewById(R.id.password);
+
+        // TODO: implemtnt button listener and remove this
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(),
-                        ProfessorMainActivity.class);
-                startActivity(intent);
+                String username = String.valueOf(usernameInput.getText());
+                String password = String.valueOf(passwordInput.getText());
+
+                if ( ! validateUserInput(username, password) ) {
+
+                    Toast.makeText(getBaseContext(), "Empty Inputs", Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(LoginActivity.this,
+                            new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if ( task.isSuccessful() ) {
+
+                                Intent intent =
+                                        new Intent(getBaseContext(),
+                                                ProfessorMainActivity.class);
+                                startActivity(intent);
+
+                            } else {
+                                Toast.makeText(getBaseContext(), "Invalide " +
+                                        "Credentials", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
             }
         });
     }
+
+    private Boolean validateUserInput(String username, String password) {
+
+        if ( username.length() == 0 )
+            return false;
+
+        if ( password.length() == 0 )
+            return false;
+
+        return true;
+    }
+
 }
